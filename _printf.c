63d28d16ab694b53537bbddda48c76d82fac2c14
Efa Tariku <efatariku07@gmail.com>
2023-10-16 18:29:11 +0300

    debuged and updated

---
+++ _printf.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * _printf - is a function that selects the correct function to print.
+ * @format: identifier to look for.
+ * Return: the length of the string.
+ */
+int _printf(const char * const format, ...)
+{
+	convert_match m[] = {
+		{"%s", print_str}, {"%c", print_char},
+		{"%%", print_37},
+		{"%i", print_int}, {"%d", print_d}, {"%r", print_s_r},
+		{"%R", print_rot13}, {"%b", print_b}, {"%u", print_unsigned},
+		{"%o", print_octa}, {"%x", print_hex}, {"%X", print_HEX},
+		{"%S", print_exl_str}, {"%p", print_po}
+	};
+
+	va_list args;
+	int i = 0, j, len = 0;
+
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
+Here:
+	while (format[i] != '\0')
+	{
+		j = 13;
+		while (j >= 0)
+		{
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			{
+				len += m[j].f(args);
+				i = i + 2;
+				goto Here;
+			}
+			j--;
+		}
+		_putchar(format[i]);
+		len++;
+		i++;
+	}
+	va_end(args);
+	return (len);
+}
