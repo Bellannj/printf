7f0cd808b073bc6181a521c67c9a4c54f4346d13
Efa Tariku <efatariku07@gmail.com>
2023-10-16 10:12:31 +0300

    updated

--- main.h
+++ main.h
@@ -1,14 +1,41 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <unistd.h>
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <limits.h>
+#include <unistd.h>
 
+
+typedef struct format
+{
+	char *id;
+	int (*f)();
+} convert_match;
+
+int print_po(va_list val);
+int print_hex_aux(unsigned long int num);
+int print_Hex_aux(unsigned int num);
+int print_exl_str(va_list val);
+int print_HEX(va_list val);
+int print_hex(va_list val);
+int print_octa(va_list val);
+int print_unsigned(va_list args);
+int print_b(va_list val);
+int print_s_r(va_list args);
+int print_rot13(va_list args);
+int print_int(va_list args);
+int print_d(va_list args);
+int _strlen(char *s);
+int *_strcpy(char *dest, char *src);
+int _strlenc(const char *s);
+int rev_string(char *s);
+int _strlenc(const char *s);
+int print_37(void);
+int print_char(va_list val);
+int print_str(va_list val);
+int _putchar(char c);
 int _printf(const char *format, ...);
-
 
 #endif
